192d8801a956ce08ca756877f831800304689523
anujedits42-byte <anujedits42@gmail.com>
2026-04-03 22:32:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@
 import os
 
 # Telegram API credentials
-API_ID =   # Replace with your API ID
-API_HASH = ""  # Replace with your API Hash
-BOT_TOKEN = ""  # Replace with your Bot Token
+API_ID = 34446649  # Replace with your API ID
+API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"  # Replace with your API Hash
+BOT_TOKEN = "8741784728:AAFLpwz7UZvEUumoxgO2I7ii8Lo-9ZSpa1o"  # Replace with your Bot Token
 
 # ----------------------------------------
 # 𝐌𝐀𝐃𝐄 𝐁𝐘 𝐀𝐁𝐇𝐈
@@ -33,13 +33,13 @@ ALLOWED_GROUP_IDS = [
 ]
 
 # Owner user ID
-OWNER_ID =   # Owner's user ID
+OWNER_ID = 7892805795  # Owner's user ID
 
 # Maximum file size (e.g., 4GB)
 MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4GB in bytes
 
 # Premium users and daily limits
-PREMIUM_USERS = {5756495153}  # Add premium user IDs here
+PREMIUM_USERS = {7892805795}  # Add premium user IDs here
 DAILY_LIMIT_FREE = 15  # Videos per day for free users
 DAILY_LIMIT_PREMIUM = 30  # Videos per day for premium users
 
